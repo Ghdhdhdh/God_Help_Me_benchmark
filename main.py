f7864b442651465472a8cb43856456df08f849d1
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-07-05 14:55:22 +1000

    Update main.py
    Made each function run 200 times for more consistency, and made the final scoe be multiplied by 1e8 to eliminate scientific notation.

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ getcontext().prec = 10
 
 modules = [fibonacci, Sieve_of_Eratosthenes]
 
-
+def sum(li:list):
+    final = 0
+    for num in li:
+        final += num
+    return final
 score = {}
 for module in modules:
-    score[module.__name__] = module.benchmark()
+    scores = []
+    for i in range(200):
+        scores.append(module.benchmark())
+    score[module.__name__] = (sum(scores) / len(scores))
 
 final_score = 1
 
 for key, value in score.items():
     final_score *= value
 
-print(f"Your final score is {final_score}")
+print(f"Your final score is {round(final_score*100000000, 1)}")
 
 for key, value in score.items():
-    print(f"You scored {value:.1000000f} in {key}")
+    print(f"You scored {value} in {key}")
